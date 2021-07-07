1826d099fad246af4ed087c327fa3775ebe2441a
C.T. Bell <charlestbell@gmail.com>
2021-07-07 16:07:36 -0400

    Rearranging files

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const CategoryMealScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Category Meal Screen!</Text>
+      <Button
+        title='Go to Details!'
+        onPress={() => {
+          props.navigation.navigate({ routeName: 'MealDetail' });
+        }}
+      />
+      <Button
+        title='Go Back'
+        onPress={() => {
+          props.navigation.goBack();
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default CategoryMealScreen;
