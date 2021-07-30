99e376f699f54a382885193d03aac06950070d3c
C.T <ct.bell@yahoo.com>
2021-07-29 22:32:12 -0400

    Making categories clickable. Adding Header styles.

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
 
 const CategoryMealScreen = (props) => {
+  const catId = props.navigation.getParam("categoryId");
+
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen!</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button
-        title='Go to Details!'
+        title="Go to Details!"
         onPress={() => {
-          props.navigation.navigate({ routeName: 'MealDetail' });
+          props.navigation.navigate({ routeName: "MealDetail" });
         }}
       />
       <Button
-        title='Go Back'
+        title="Go Back"
         onPress={() => {
           props.navigation.goBack();
         }}
@@ -24,8 +31,8 @@ const CategoryMealScreen = (props) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
