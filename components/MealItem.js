6468cb7c48ed5bfc46933e91c1c972d8f391ae63
C.T <ct.bell@yahoo.com>
2021-08-03 10:17:11 -0400

    Adding meal details

---
+++ components/MealItem.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+const MealItem = (props) => {
+  return (
+    <View style={styles.mealItem}>
+      <TouchableOpacity onPress={props.onSelectMeal}>
+        <View>
+          <View style={{ ...styles.mealRow, ...styles.mealHeader }}>
+            <Text>{props.title}</Text>
+          </View>
+          <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
+            <Text>{props.duration}m</Text>
+            <Text>{props.complexity}</Text>
+            <Text>{props.affordability}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mealItem: {
+    height: 200,
+    width: "100%",
+    backgroundColor: "#ccc",
+  },
+  mealRow: {
+    flexDirection: "row",
+  },
+  mealHeader: {
+    height: "90%",
+  },
+  mealDetail: {
+    paddingHorizontal: 10,
+    justifyContent: "space-between",
+  },
+});
+
+export default MealItem;
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -2,13 +2,18 @@ import React from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
 
 import { CATEGORIES, MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 const CategoryMealScreen = (props) => {
   const renderMealItem = (itemData) => {
     return (
-      <View>
-        <Text>{itemData.item.title}</Text>
-      </View>
+      <MealItem
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        title={itemData.item.title}
+        onSelectMeal={() => {}}
+      />
     );
   };
   const catId = props.navigation.getParam("categoryId");
@@ -23,6 +28,7 @@ const CategoryMealScreen = (props) => {
         data={displayedMeals}
         keyExtractor={(item, index) => item.id}
         renderItem={renderMealItem}
+        style={{ width: "100%" }}
       />
     </View>
   );
